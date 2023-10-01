a8ccf2972b6ea736916e071aaadc1f522e4e865e
JOY19ANYANGO <joyanyango402@gmail.com>
2023-10-02 00:17:08 +0300

    created and poopulated db

--- app/models.py
+++ app/models.py
@@ -1,10 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+from sqlalchemy.orm import validates
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    # Define a one-to-many relationship with HeroPower
+    powers = db.relationship('HeroPower', back_populates='hero')
+
+    
+    
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    
+    # Define the foreign key relationships
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Establish the many-to-one relationships
+    hero = db.relationship('Hero', back_populates='powers')
+    power = db.relationship('Power', back_populates='heroes')
+
+
+class Power(db.Model):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Define a one-to-many relationship with HeroPower
+    heroes = db.relationship('HeroPower', back_populates='power')
